820bdffe6946f26f3ff2eda8828cbe214b796acb
hiroya-enokida <java2daiei@gmail.com>
2024-10-02 21:05:41 +0900

    Update handle-issue.js

--- .github/script/handle-issue.js
+++ .github/script/handle-issue.js
@@ -1,3 +1,10 @@
 // handle-issue.js
 const issue = require(process.env.GITHUB_EVENT_PATH);
 console.log('Issue created: ' + issue.issue.title);
+
+// ブラウザ環境でアラートを表示する場合
+if (typeof window !== 'undefined') {
+  alert('テストメッセージ');
+} else {
+  console.log('テストメッセージ');
+}
